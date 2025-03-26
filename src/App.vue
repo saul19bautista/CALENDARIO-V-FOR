60496784a77f18549487c63e8f145db877ccb9cf
Saul <lopezbautistasaul12@gmail.com>
2025-03-25 19:29:11 -0600

    Horario docente

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@ import HelloWorld from './components/HelloWorld.vue'
 import Recorrido from './components/Recorrido.vue'
 import Misdatos from './components/Misdatos.vue'
 import Horario from './components/Horario.vue'
+import HorarioDocente from './components/HorarioDocente.vue'
 </script>
 <template>
    <!--<HelloWorld msg="Vite + Vue" />-->
    <!--Recorrido />-->
    <!--Misdatos />-->
-   <Horario />
+   <!--Horario />-->
+   <HorarioDocente />
 </template>
 
 <style scoped>
